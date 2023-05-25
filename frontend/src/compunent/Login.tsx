b3d3b4b4409cc1c4ea71a9cdef3d5ac11e999a93
ali-tevfik <tevfikdoner@hotmail.com>
2023-05-25 12:39:55 +0200

    base

--- frontend/src/compunent/Login.tsx
+++ frontend/src/compunent/Login.tsx
@@ -1,5 +1,48 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+// import { OAuth2Client } from 'oauth2-client';
+
+
+
+
+
+
+// const clientId = 'u-s4t2ud-abe60458fc8bed380ffb209bcfe0eaaf653fa06e185f7ac4f0fb307132134bfe';
+// const clientSecret = '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';
+// const authorizationUrl = 'http://localhost:3000';
+
+// const oauth2Client = new OAuth2Client({
+//   clientId,
+//   clientSecret,
+//   authorizationUrl,
+// });
+
+// // Kullanıcıyı yetkilendirme sayfasına yönlendirme
+// function redirectToAuthorization() {
+//   const authorizationUri = oauth2Client.getAuthorizationUri();
+//   window.location.href = authorizationUri;
+// }
+
+// // Yetkilendirme kodunu alıp erişim tokenini alma
+// async function exchangeCodeForToken(code: string) {
+//   const token = await oauth2Client.getToken(code);
+//   // Erişim tokeni ile işlemler yapabilirsiniz
+//   console.log('Access Token:', token.access_token);
+// }
+
+// // Örnek kullanım
+// redirectToAuthorization(); // Kullanıcıyı yetkilendirme sayfasına yönlendirir
+
+// // Yönlendirme sonrası, URL parametresinden yetkilendirme kodunu alarak token alımını gerçekleştirir
+// const urlParams = new URLSearchParams(window.location.search);
+// const code = urlParams.get('code');
+// if (code) {
+//   exchangeCodeForToken(code);
+// }
+
+
+
+
 
 
 const Login = () => {
@@ -60,6 +103,13 @@ const Login = () => {
           <button className="LoginButton" onClick={handleClick}>
             Login
           </button>
+          <button>
+            
+         <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-abe60458fc8bed380ffb209bcfe0eaaf653fa06e185f7ac4f0fb307132134bfe&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code ">
+      42 Intra Login
+    </a>
+  </button>
+
         </div>
       </div>
     </div>

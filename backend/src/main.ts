72b9ee1ac7d926868e641a98fe27bb4499c2e3e2
ali-tevfik <tevfikdoner@hotmail.com>
2023-06-08 19:30:45 +0200

    Make logout and status

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as dotenv from 'dotenv';
 
+import { ConfigService } from '@nestjs/config';
+
 
 
 
@@ -13,19 +15,27 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://api.intra.42.fr/auth/authorize'],
-    methods: 'GET, POST',
+    origin: ['http://localhost:3000'],
+    credentials: true,
   });
-  
 
-  app.use(
-    session({
-      secret: 'top',
-      resave: false,
-      saveUninitialized: false,
-    }),
-  );
 
-await app.listen(3000);
+	const configService = app.get(ConfigService);
+	app.use(
+		session({
+			cookie: {
+				maxAge: 6000 * 60 * 24, // login cookie is 24 hours valid
+			},
+			secret: configService.get("secretKey"),
+			resave: false,
+			saveUninitialized: false,
+		}),
+	);
+
+  const passport = require('passport');
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  await app.listen(3000);
   }
 bootstrap();
